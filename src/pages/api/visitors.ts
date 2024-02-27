541b40ddbfd5518bf4bf402e82043a3639c8ef40
Bablu202 <102294921+Bablu202@users.noreply.github.com>
2024-02-27 21:01:27 +0100

    Update visitors.ts
    visitors - n

--- src/pages/api/visitors.ts
+++ src/pages/api/visitors.ts
@@ -8,8 +8,21 @@ export default async function handler(
   res: NextApiResponse<any>
 ) {
   if (req.method === "GET") {
-    const visitors = await prisma.visitors.findMany();
+    const visitors = await prisma.visitors.findUnique({
+      where: { id: 1 },
+    });
     res.status(200).json(visitors);
   }
   res.status(200).json({ id: 1 });
+
+  if (req.method === "PUT") {
+    const id = req.query.id;
+    const { count } = req.body;
+
+    const visitors = await prisma.visitors.update({
+      where: { id: 1 },
+      data: { count: count },
+    });
+    res.status(200).json(visitors);
+  }
 }
