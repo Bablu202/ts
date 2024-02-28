f7a9800f514982712a5ac8f34f6ce2dcf4112e85
Bablu202 <102294921+Bablu202@users.noreply.github.com>
2024-02-28 15:14:45 +0100

    Update Count.tsx

--- src/pages/api/Count.tsx
+++ src/pages/api/Count.tsx
@@ -7,20 +7,36 @@ const Count: NextPage = () => {
   const [dataVisitors, setDataVisitors] = useState<number>();
   const fetcher = (url: string) => fetch(url).then((data) => data.json());
   const { data, error, isLoading, mutate } = useSWR("/api/visitors", fetcher);
-
+  if (!isLoading) console.log("fetcher from 10 ", data);
   const handleVisitorUpdate = async () => {
     const id = 1;
+    const response = await fetch(`/api/visitors?id=${id}`);
 
+    if (!response.ok) {
+      console.error("Error fetching data:", response.statusText);
+      return;
+    }
+    const data = await response.json();
+    console.log(data);
+    const updatedCount = data.count + 1;
     const updateResponse = await fetch(`/api/visitors?id=${id}`, {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(visitors}),
+      body: JSON.stringify({ count: updatedCount }),
     });
- 
+    if (!updateResponse.ok) {
+      console.error("Error updating count:", updateResponse.statusText);
+      return;
+    }
+
+    return updateResponse.json();
+
+    // Optionally, you can refetch the data to ensure it's up to date
     mutate();
   };
+
   useEffect(() => {
     if (data) {
       setDataVisitors(data.count);
